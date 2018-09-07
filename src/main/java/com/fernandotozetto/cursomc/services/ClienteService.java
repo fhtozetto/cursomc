c287e72441e3cdfefe79128dafa2ad010316f698
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-09-07 16:13:45 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/fernandotozetto/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.fernandotozetto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernandotozetto.cursomc.domain.Cliente;
+import com.fernandotozetto.cursomc.repositories.ClienteRepository;
+import com.fernandotozetto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired // cria o objeto automaticamente por injeção de dependência ou inversão de controle.
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
