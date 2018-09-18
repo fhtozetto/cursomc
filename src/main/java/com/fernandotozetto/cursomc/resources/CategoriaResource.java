dda632144a2454e07b4974cf269116a1eb779531
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-09-18 20:19:54 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/fernandotozetto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fernandotozetto/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,21 @@
 package com.fernandotozetto.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernandotozetto.cursomc.domain.Categoria;
 import com.fernandotozetto.cursomc.services.CategoriaService;
 
-@RestController
-@RequestMapping(value="/categorias")
+@RestController // Marca a classe como controladora rest
+@RequestMapping(value="/categorias") // EndPoint Rest
 public class CategoriaResource {
 	
 	@Autowired // cria o objeto automaticamente por injeção de dependência ou inversão de controle.
@@ -22,6 +26,13 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
