26b349a3f177f716c87e22ea30eff5268830ae21
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-10-15 11:37:05 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/fernandotozetto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fernandotozetto/cursomc/resources/CategoriaResource.java
@@ -39,7 +39,7 @@ public class CategoriaResource {
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
 		Categoria obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objDTO.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
--- src/main/java/com/fernandotozetto/cursomc/resources/ClienteResource.java
+++ src/main/java/com/fernandotozetto/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.fernandotozetto.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernandotozetto.cursomc.domain.Cliente;
 import com.fernandotozetto.cursomc.dto.ClienteDTO;
+import com.fernandotozetto.cursomc.dto.ClienteNewDTO;
 import com.fernandotozetto.cursomc.services.ClienteService;
 
 @RestController
@@ -25,6 +28,14 @@ public class ClienteResource {
 	
 	@Autowired // cria o objeto automaticamente por injeção de dependência ou inversão de controle.
 	private ClienteService service;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
