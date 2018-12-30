fcb8704231e03853dcdd2356b4e52f6272310a3e
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-12-30 16:06:44 -0300

    Esqueci a senha

--- src/main/java/com/fernandotozetto/cursomc/resources/AuthResource.java
+++ src/main/java/com/fernandotozetto/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.fernandotozetto.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fernandotozetto.cursomc.dto.EmailDTO;
 import com.fernandotozetto.cursomc.security.JWTUtil;
 import com.fernandotozetto.cursomc.security.UserSS;
+import com.fernandotozetto.cursomc.services.AuthService;
 import com.fernandotozetto.cursomc.services.UserService;
 
 @RestController
@@ -23,12 +27,21 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@RequestMapping(value="/refresh_token", method=RequestMethod.POST) // EndPoint protegido por altenticação só é possivel acessar com o token válido.
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 	UserSS user = UserService.authenticated(); // Busca o usuario logado.
 	String token = jwtUtil.generateToken(user.getUsername()); //Gera um novo token com o usuario logado. (Atualiza o Token)
 	response.addHeader("Authorization", "Bearer " + token); // adiciona o token na resposta da requisição
 	return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST) // esqueci minha senha
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
 	}
 
 }
