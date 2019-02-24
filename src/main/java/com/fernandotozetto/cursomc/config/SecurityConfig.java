c9130578ef87c43ce0c109a20899d21ba3a87af4
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2019-02-24 18:48:19 -0300

    Enviando imagem via endpoint

--- src/main/java/com/fernandotozetto/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fernandotozetto/cursomc/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = { // caminhos que estão liberados apenas para Cadastro
-			"/clientes/**",
+			"/clientes",
+			"/clientes/picture",
 			"/auth/forgot/**"
 	};
 
